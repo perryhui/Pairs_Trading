2492edad7a3631b7b2a1007f6af0f3b3c758829b
perryhui <115354208+perryhui@users.noreply.github.com>
2025-01-18 22:40:06 +0000

    Data Preprocessing

--- Read_and_process_ticker_data.py
+++ Read_and_process_ticker_data.py
@@ -1,34 +1,55 @@
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
+import random
+import csv
+
+price_path = 'ticker_data_2010-01-01_2023-12-31.csv'
+ratio_path = 'financial_ratio_data_2010-01-01_2023-12-31.csv'
+
+
 # Load the csv into python for data processing
-price_df = pd.read_csv('ticker_data_2010-01-01_2023-12-31.csv', low_memory= False)
-volume_df = pd.read_csv('volume_data_2010-01-01_2023-12-31.csv', low_memory= False)
-ratio_df = pd.read_csv('financial_ratio_data_2010-01-01_2023-12-31.csv', low_memory= False)
+price_df = pd.read_csv(price_path, low_memory= False)
+ratio_df = pd.read_csv(ratio_path, low_memory= False)
+
+data_frames = [price_df, ratio_df]
+
+def Data_frame_processor(file_path):
+    # Clean up some naming issues and combine the first two entries in every column
+    df = pd.read_csv(file_path, low_memory= False, header= [0, 1])
+    df.columns = [' '.join([str(col1).strip(), str(col2).strip()]) if col1 else str(col2).strip() 
+                  for col1, col2 in zip(df.columns.get_level_values(0), df.columns.get_level_values(1))]
+    df.rename(columns={'Unnamed: 0_level_0 Unnamed: 0_level_1': 'Date'}, inplace=True)
+
+    # Disgard any column of data with more than 5% of missing value in the given time frame
+    
+
+
+    return df
+
+
+
+price_cleaned_df = Data_frame_processor(price_path)
+ratio_cleaned_df = Data_frame_processor(ratio_path)
+
+
+
+
 
 '''
 While there are imputation methodlogies used to fill in missing values in the data frame, 
 it inevidently distorts the true distribution of data. Say for a example we have over 50% of data missing
 from a stock, simple imputation methods with mean, mode, median assume that imputed values are similar to that
 in the data set. We shall remove stocks with more than 5% of data in the given time frame
+
+
+# Data Imputation: We use linear regression and stochastic imputation to impute the missing values 
+# We spilt the dataframe into to sub-dataframes: 
+# Rows without missing values (to train the regression model).
+# Rows with missing values (to predict the missing data).
+# Stochastic imputation is used to adding randomness in the imputed data
 '''
-
-# Calculate the percentage of missing price values in each column (stock)
-price_missing_percentage = price_df.isnull().mean() * 100
-
-# Filter out columns with more than 5% missing values
-price_df_cleaned = price_df.loc[:, price_missing_percentage <= 5]
-
-#Do the same for financial ratio 
-
-ratio_missing_percentage = ratio_df.isnull().mean() * 100
-
-ratio_df_cleaned = ratio_df.loc[:, ratio_missing_percentage <= 5]
-
-# combine cleaned price df with volume data
-price_vol_df_cleaned = price_df_cleaned.join(volume_df)
-
-#Data Imputation
-
 
 
 
