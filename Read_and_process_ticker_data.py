2923930970aac5eab3674d9561005755981e9365
perryhui <115354208+perryhui@users.noreply.github.com>
2025-01-18 09:42:59 +0000

    18-1-2025 update

--- Read_and_process_ticker_data.py
+++ Read_and_process_ticker_data.py
@@ -1,26 +1,35 @@
 import pandas as pd
 import numpy as np
-#Load the csv into python for data processing
-df = pd.read_csv('ticker_data_2010-01-01_2023-12-31.csv', low_memory= False)
+# Load the csv into python for data processing
+price_df = pd.read_csv('ticker_data_2010-01-01_2023-12-31.csv', low_memory= False)
+volume_df = pd.read_csv('volume_data_2010-01-01_2023-12-31.csv', low_memory= False)
+ratio_df = pd.read_csv('financial_ratio_data_2010-01-01_2023-12-31.csv', low_memory= False)
 
 '''
 While there are imputation methodlogies used to fill in missing values in the data frame, 
 it inevidently distorts the true distribution of data. Say for a example we have over 50% of data missing
 from a stock, simple imputation methods with mean, mode, median assume that imputed values are similar to that
-in the data set. Given that data is from Bloomberg, the only reason for missing data is perhaps that the stock has not been listed 
-for the entirety of the time frame. Hence we shall remove stocks with more than 5% of data in the given time frame
+in the data set. We shall remove stocks with more than 5% of data in the given time frame
 '''
 
-# Calculate the percentage of missing values in each column (stock)
-missing_percentage = df.isnull().mean() * 100
+# Calculate the percentage of missing price values in each column (stock)
+price_missing_percentage = price_df.isnull().mean() * 100
 
 # Filter out columns with more than 5% missing values
-df_cleaned = df.loc[:, missing_percentage <= 5]
+price_df_cleaned = price_df.loc[:, price_missing_percentage <= 5]
 
-'''
-We have filtered out 84 stocks that do not satisfy the criteria
-We shall now use imputation to fill in the missing value
+#Do the same for financial ratio 
 
-'''
+ratio_missing_percentage = ratio_df.isnull().mean() * 100
+
+ratio_df_cleaned = ratio_df.loc[:, ratio_missing_percentage <= 5]
+
+# combine cleaned price df with volume data
+price_vol_df_cleaned = price_df_cleaned.join(volume_df)
+
+#Data Imputation
+
+
+
 
 
--- pull_historical_FTSE.py
+++ pull_historical_FTSE.py
@@ -2,43 +2,59 @@ import blpapi
 from xbbg import blp
 import pandas as pd
 
+ftse_tickers = [
+    "ENT", "SMIN", "SPX", "PRU", "AAL", "CRDA", "BEZ", "TW", "GLEN", "AHT", "DPLM", 
+    "IMI", "RKT", "DCC", "HSX", "EXPN", "SMT", "DGE", "BARC", "BKG", "ABF", "LLOY", 
+    "RIO", "MNDI", "WPP", "LAND", "PSN", "SMDS", "BATS", "MRO", "NWG", "HWDN", "ULVR", 
+    "RR", "KGF", "WEIR", "IAG", "GAW", "NG", "ANTO", "LMP", "FCIT", "CCH", "BNZL", 
+    "ICG", "UTG", "IHG", "BT.A", "SBRY", "EZJ", "SGRO", "BP", "REL", "INF", "MNG", 
+    "PHNX", "HLMA", "SHEL", "SDR", "ALW", "TSCO", "HLN", "CNA", "NXT", "STAN", "AZN", 
+    "STJ", "BLND", "UU", "BA", "PSON", "GSK", "AV", "ITRK", "SGE", "HIK", "RTO", "PSH", 
+    "BTRW", "HSBA", "WTB", "ADM", "SSE", "CTEC", "RMV", "LGEN", "SN", "EDV", "III", 
+    "SVT", "IMB", "VOD", "CPG", "HL", "AAF", "LSEG", "AUTO", "MKS", "FRES", "JD",
+    "BYG", "FXPO", "AML", "DSCV", "CCL", "QLT", "SAFE", "N91", "WG", "HWG", "RS1", 
+    "DOCS", "TRST", "WIZZ", "CMCX", "COA", "PHI", "ITH", "IWG", "WKP", "FCSS", "SXS", 
+    "INVP", "ROR", "THRG", "SRE", "BWY", "PHLL", "FGT", "BBOX", "VCT", "AJB", "IEM", 
+    "PLUS", "KNOS", "SHED", "CKN", "OXIG", "STEM", "GPE", "SHC", "CWK", "GNC", "FAN", 
+    "IHP", "BOY", "RHIM", "PPH", "MGAM", "HAS", "JMAT", "HET", "ABDN", "DLN", "HBR", 
+    "CRST", "TPK", "HTG", "ZIG", "CCR", "EDIN", "SMWH", "GRI", "PPET", "DWL", "EMG", 
+    "PAGE", "IGG", "RSW", "PHP", "SRP", "PTEC", "HFG", "SSON", "GNS", "BME", "GRG", 
+    "MNKS", "TATE", "LRE", "ESP", "TEM", "CCC", "MYI", "PRSR", "PCT", "FEV", "BAG", 
+    "DNLM", "INCH", "ALPH", "JEDT", "CURY", "CGT", "OSB", "BBY", "SVS", "HSL", "MRCH", 
+    "EOT", "JMG", "ATG", "BRGE", "PAG", "CTY", "JAM", "LWDB", "JGGI", "HILS", "BRSC", 
+    "SSPG", "ITV", "ELM", "BBH", "MUT", "OCDO", "MRC", "AGT", "BRWM", "SAIN", "SDP", 
+    "BOWL", "FUTR", "NESF", "GFTU", "THRL", "IBST", "ATT", "HTWS", "IDS", "HMSO", "FGP", 
+    "VEIL", "TRY", "TMPL", "TIFS", "SYNC", "SPT", "SOI", "PNN", "PFD", "PCFT", "MSLH", 
+    "MAB", "JDW", "GSCT", "DLG", "DGN", "BREE", "BMY", "ALFA", "AGR", "ENOG", "RAT", 
+    "QQ", "KIE", "NCC", "CHG", "ESNT", "BNKR", "BYIT", "USA", "PNL", "MOON", "RCP", 
+    "ASHM", "FSG", "DOM", "BGFD", "BCG", "WWH", "PIN", "FEML", "3IN", "SCT", "JFJ", "TEP", 
+    "MTO", "VSVS", "RICA", "HVPE", "HGT", "MGNS", "JTC", "SUPR", "BSIF", "FSV", "DRX", 
+    "RWI", "NBPE", "FSFL", "BAB", "ICGT", "SNR", "TBCG", "GEN", "TRN", "MONY", "ASL", 
+    "HICL", "VTY", "BRBY", "MCG", "GDWN", "CLDN", "SPI", "PAY", "INPP", "TCAP", "GROW", 
+    "KLR", "VOF", "BUT", "XPS", "BGEO", "FRAS", "MTRO", "TFIF", "JII", "FOUR", "APAX", 
+    "UKW", "MEGP", "BBGI", "WOSG", "SEIT", "RPI", "JUST", "FGEN", "EWI", "AO", "ROO", 
+    "SEQI", "GCP", "BHMG", "DEC", "PETS", "CHRY", "ESCT", "IPO", "ONT", "JUP", "TRIG", 
+    "BPT", "NAS", "WPS", "HOC", "HRI", "BAKK", "BVIC","AGVI", "AAS", "AAIF", "ADIG", "AEI", "ASLI", "ANII", "API", "AUSC", "AEWU", "AEP", "APN", "APTD", 
+    "AIE", "ASC", "AUGM", "ARR", "AJOT", "AVON", "BGCG", "BGEU", "BGS", "BGUK", "BIOG", "BRAI", "BERI", 
+    "BRFI", "BRLA", "BOOT", "BASC", "CABP", "CPI", "CAPD", "CNE", "CARD", "CRT", "CARR", "CCJI", "CWR", 
+    "CSN", "CLIG", "CBG", "CLI", "COST", "CYN", "NCYF", "CTPE", "CTUK", "CHI", "CREI", "CVCG", "DLAR", 
+    "DFS", "DGI9", "DIVI", "DORE", "DIG", "EGL", "ECOR", "ENQ", "ECEL", "EAT", "EVOK", "FDM", "FAS", 
+    "FJV", "FORT", "FOXT", "FSTA", "FCH", "GFRD", "GABI", "GOT", "GSF", "GMS", "GYM", "HFD", "HEAD", 
+    "HLCL", "HFEL", "HHI", "HINT", "HOT", "HSW", "IGC", "IBT", "IPF", "IAT", "BIPS", "IGET", "IPU", "FSJ", 
+    "JAGI", "JCGI", "JCH", "JEGI", "JARA", "JEMI", "JUGI", "JUSC", "KMR", "KPC", "LABS", "LTI", "LIO", 
+    "LWI", "LSL", "LUCE", "MGCI", "MACF", "MAJE", "MNL", "MARS", "MNP", "MCB", "MER", "MWY", "MCT", "GLE", "MMIT", 
+    "MTE", "MTU", "MOTR", "NRR", "NAVF", "NXR", "NAIT", "OCN", "ORIT", "OIT", 
+    "OTB", "OIG", "OXB", "PAC", "PCA", "PINT", "PBEE", "PDL", "PFC", "PHAR", 
+    "PSDL", "PCTN", "PINE", "PCGH", "PRV", "PRTC", "PZC", "RNK", "RCH", "RECI", 
+    "REC", "RGL", "RESI", "RCDO", "RIII", "RSE", "RMII", "RWA", "SUS", "SBRE", 
+    "SAGA", "ATR", "SERE", "SCF", "SJG", "SREI", "SCP", "INOV", "SST", "STB", 
+    "SSIT", "SFR", "SHRS", "SHI", "SNWS", "SOHO", "SDY", "SWEF", "SEC", "STS", 
+    "STVG", "SYNT", "TMIP", "TPT", "TET", "TRI", "TTG", "TLW", "SMIF", "ULTP", 
+    "UEM", "VIP", "VANQ", "ENRG", "VID", "VNH", "VP.", "VSL", "WHR", "WIX", "XAR", 
+    "XPP", "ZTF"
+]
 
-ftse_tickers = ["ADM", "AAF", "ALW", "AAL", "ANTO", "AHT", "ABF", "AZN", "AUTO", "AV", 
-    "BA", "BARC", "BTRW", "BEZ", "BKG", "BP", "BATS", "BLND", "BT-A", "BNZL",
-    "CNA", "CCH", "CPG", "CTEC", "CRDA", "DCC", "DGE", "DPLM", "EDV", "ENT", 
-    "EZJ", "EXPN", "FCIT", "FRES", "GAW", "GLEN", "GSK", "HLN", "HLMA", "HL",
-    "HIK", "HSX", "HWDN", "HSBA", "IHG", "IMI", "IMB", "INF", "ICG", "IAG", 
-    "ITRK", "JD", "KGF", "LAND", "LGEN", "LLOY", "LMP", "LSEG", "MNG", "MKS", 
-    "MRO", "MNDI", "NG", "NWG", "NXT", "PSON", "PSH", "PSN", "PHNX", "PRU", 
-    "RKT", "REL", "RTO", "RMV", "RIO", "RR", "SGE", "SBRY", "SDR", "SMT", 
-    "SGRO", "SVT", "SHEL", "SMDS", "SMIN", "SN", "SPX", "SSE", "STAN", 
-    "STJ", "TW", "TSCO", "ULVR", "UU", "UTG", "VOD", "WEIR", "WTB", "WPP", 
-    "3IN", "FOUR", "ABDN", "ALFA", "ATT", "ALPH", "AO", "APAX", "ASHM", "DGN",
-    "AGR", "AML", "ATG", "AGT", "BME", "BAB", "BGFD", "USA", "BAKK", "BBY", 
-    "BCG", "BNKR", "BGEO", "BAG", "BBGI", "AJB", "BBH", "BWY", "BHMG", "BYG", 
-    "BRGE", "BRSC", "THRG", "BRWM", "BMY", "BSIF", "BOY", "BREE", "BPT", "BVIC", 
-    "BUT", "BRBY", "BYIT", "CCR", "CLDN", "CGT", "CCL", "CHG", "CHRY", "CTY", 
-    "CKN", "CMCX", "COA", "CCC", "CWK", "CRST", "CURY", "ROO", "DLN", "DLG", 
-    "DSCV", "DEC", "DOM", "DWL", "DRX", "DOCS", "DNLM", "EDIN", "EWI", "ELM", 
-    "ESP", "ENOG", "ESNT", "EOT", "ESCT", "FXPO", "FCSS", "FEML", "FEV", "FSV", 
-    "FGT", "FGP", "FSG", "FSFL", "FRAS", "FUTR", "GCP", "GEN", "GNS", "GSCT", 
-    "GDWN", "GFTU", "GRI", "GPE", "UKW", "GNC", "GRG", "HMSO", "HBR", "HVPE", 
-    "HWG", "HAS", "HTWS", "HET", "HSL", "HRI", "HGT", "HICL", "HILS", "HFG", 
-    "HOC", "BOWL", "HTG", "IBST", "ICGT", "IGG", "IEM", "INCH", "IHP", "IDS", 
-    "INPP", "INVP", "IPO", "ITH", "ITV", "IWG", "JLEN", "JMAT", "JAM", "JMG", 
-    "JEDT", "JGGI", "JII", "JFJ", "JTC", "JUP", "JUST", "KNOS", "KLR", "KIE", 
-    "LRE", "LWDB", "EMG", "MSLH", "MEGP", "MRC", "MRCH", "MTRO", "MAB", "MTO", 
-    "MCG", "GROW", "MONY", "MNKS", "MOON", "MGAM", "MGNS", "MUT", "MYI", "NBPE", 
-    "NCC", "NESF", "N91", "NAS", "OCDO", "OSB", "OXIG", "ONT", "PHI", "PAGE", 
-    "PIN", "PAG", "PPET", "PAY", "PNN", "PNL", "PHLL", "PETS", "PTEC", "PLUS",
-    "PCFT", "PCT", "PPH", "PFD", "PHP", "PRS", "QQ", "QLT", "RPI", "RAT", 
-    "RWI", "RSW", "RHIM", "RCP", "ROR", "RS1", "RICA", "SAFE", "SVS", "SDP", 
-    "SOI", "SAIN", "SEIT", "SNR", "SEQI", "SRP", "SHC", "SRE", "SSON", "SCT", 
-    "SXS", "SPI", "SPT", "SSPG", "STEM", "SUPR", "SYNC", "THRL", "TATE", "TBCG",
-    "TEP", "TMPL", "TEM", "TRIG", "TIFS", "TCAP", "TRN", "TPK", "BBOX", "TRY", 
-    "TRST", "TFIF", "SHED", "VSVS", "VCT", "VEIL", "VOF", "VTY", "FAN", "WPS", 
-    "WOSG", "JDW", "SMWH", "WIZZ", "WG", "WKP", "WWH", "XPS", "ZIG"
-]  
+
 
 def data_processing(ftse_tickers):
     ftse_updated = []
@@ -49,17 +65,14 @@ def data_processing(ftse_tickers):
 
 ftse_updated = data_processing(ftse_tickers)
 
-fields_price = ['Last_Price']
-fields_volume = ['VOLUME', 'ADVOLUME', 'VOLUME_3M', 'VOLUME_TODAY']
-fields_ratio = [
-    'PE_RATIO', 'PB_RATIO', 'DIVIDEND_YIELD', 'ROE', 'ROA', 'DEBT_TO_EQUITY'
+fields_price = ['Open', 'Close', 'Adjusted_Close' 'Last_Price', 'Volume']
+fields_ratio = ['PE_RATIO', 'PB_RATIO', 'DIVIDEND_YIELD', 'ROE', 'ROA', 'DEBT_TO_EQUITY'
 ]
 Start_date = '2010-01-01'
 End_date = '2023-12-31'
 
 # Fetch historical data
 historical_tick_data = blp.bdh(tickers=ftse_updated, flds=fields_price, start_date=Start_date, end_date=End_date)
-historical_volume = blp.bdh(tickers = ftse_updated, flds = fields_volume, start_date = Start_date, end_date = End_date)
 historical_ratio = blp.bdh(tickers = ftse_updated, flds = fields_ratio, start_date = Start_date, end_date = End_date)
 
 
@@ -67,10 +80,9 @@ historical_ratio = blp.bdh(tickers = ftse_updated, flds = fields_ratio, start_da
 file_name_price = f'ticker_data_{Start_date}_{End_date}.csv'
 historical_tick_data.to_csv(file_name_price)
 
-file_name_volume = f'volume_data_{Start_date}_{End_date}.csv'
-historical_volume.to_csv(file_name_volume)
+
 
 file_name_ratio = f'financial_ratio_data_{Start_date}_{End_date}.csv'
-historical_volume.to_csv(file_name_ratio)
+historical_ratio.to_csv(file_name_ratio)
 
 
